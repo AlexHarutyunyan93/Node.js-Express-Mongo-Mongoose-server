d6fa253ca61fa0edc1d6a3ddc7f7b23515f920f2
AlexHarutyunyan93 <47464816+AlexHarutyunyan93@users.noreply.github.com>
2019-09-20 11:08:19 +0400

    Create index.js
    index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const express         = require('express');
+const mongoose        = require('mongoose');
+const bodyParser      = require('body-parser');
+
+const app             = express();
+var cors = require('cors');
+
+
+mongoose.connect('mongodb://localhost/fashion-db',  { useNewUrlParser: true });
+
+app.use(express.static('./public'));
+
+app.use(cors({
+    'allowedHeaders': ['sessionId', 'Content-Type'],
+    'exposedHeaders': ['sessionId'],
+    'origin': '*',
+    'methods': 'GET,PUT,POST,DELETE',
+    'preflightContinue': false
+}));
+
+app.use(bodyParser.json());
+app.use('/api', require('./api'));
+
+app.listen(4000, () => {
+    console.log('Server is listening on port 4000');
+});
